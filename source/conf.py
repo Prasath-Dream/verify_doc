063fe1201298d3ebd39cee2cf6d87e19fa324d6e
Prasath-Dream <Prasath-Dream@users.noreply.github.com>
2026-02-21 11:40:44 +0530

    Update docs

---
+++ source/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = """RB-RA8D1
+QUick Start Guide"""
+copyright = '2025, PHYTEC'
+author = 'PHYTEC'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'en'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+html_title = 'RB-RA8D1'
+
+# Generated RST files: rstfile1
+
+html_css_files = ['custom_overrides.css']
---
+++ source/index.rst
@@ -0,0 +1,13 @@
+RB-RA8D1 - QUick Start Guide
+============================
+
+This documentation provides a comprehensive, step-by-step Quick Start Guide for the Renesas RB-RA8D1 microcontroller evaluation kit. It covers everything from unpacking the kit to running AI demos, with simple explanations, tables, and tips for users of all levels. Based on official manuals, it includes sections on hardware architecture, peripherals, expansion boards, and troubleshooting.
+
+.. START:auto_toctrees
+
+.. toctree::
+   :maxdepth: 2
+
+   rstfile1
+
+.. END:auto_toctrees
